70919454b309af65d1c79fd8e30e68c19729d2c9
Wesley Carr <surfswell17@gmail.com>
2013-07-31 14:34:36 -0400

    Search button implemented.

--- moviedb/movies.js
+++ moviedb/movies.js
@@ -5,6 +5,16 @@ $(function(){
     return false; //stops form from being submitted
   });
 
+  $('#search').click(function(event){
+    // to Search
+    var movieTitle = "http://www.omdbapi.com/?t="+ $('#movie').val();
+    // Send request with Ajax using movieTitle as url
+    var http = $.ajax({
+      type: 'get',
+      url: movieTitle
+    })
+  })
+
 })
 
 // $.ajax({
